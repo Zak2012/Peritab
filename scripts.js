752e8342353b79ab880fa453351ab8e2c72d7421
gsproduction727 <gsproduction727@gmail.com>
2021-08-31 19:51:50 +0800

    Updated Info section, added install button and a bit of animation

--- scripts.js
+++ scripts.js
@@ -1,14 +1,20 @@
 var scoreObj = 0;
 var highScoreObj = 0;
 var frameObj = 0;
+var homebtnObj = 0;
+var installBtnObj = 0;
+var resetbtnObj = 0;
 var root = 0;
 
-var isLeaving = 0;
 var isOnline = 0;
 var score = localStorage.getItem("Score");
 var highScore = localStorage.getItem("HighScore");
 
-const MENU = ["/settings", "/info", "/game"]
+const buttonPressMs = 200;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 function pad(num) {
     var s = "00000000" + num;
@@ -24,27 +30,67 @@ function root_set(property, value) {
     root.style.setProperty(property, value);
 }
 
-function home() {
-    frameObj.src = "/game"
+
+async function home() {
+    homebtnObj.style.backgroundColor = "#ffffff88"
+    homebtnObj.style.filter = "none"
+    homebtnObj.style.webkitFilter = "none"
+    window.location.replace("/#main")
+    frameObj.src = "/game/"
+    await sleep(buttonPressMs)
+    homebtnObj.style.backgroundColor = "#b0b0b088"
+    homebtnObj.style.filter = "invert()"
+    homebtnObj.style.webkitFilter = "invert()"
 }
 
-function confirmReset() {
+async function confirmReset() {
+    resetbtnObj.style.backgroundColor = "#ffaaaa"
+    await sleep(buttonPressMs)
+    resetbtnObj.style.backgroundColor = "#ff5555"
+    await sleep(10)
     let reset = confirm("You sure you want to reset all? \n\nCaution: This action cannot be undone");
     if (reset) {
         let reset2 = confirm("Do you sure you want to continue? \n\nCaution: This action cannot be undone");
         if (reset2) {
-            localStorage.clear();
-            sessionStorage.clear();
+            localStorage.setItem("Score", "0");
+            localStorage.setItem("HighScore", "0");
+            localStorage.setItem("isLeaving", "false")
             window.location.reload()
             window.location.href = "/"
         }
     }
 }
 
+let deferredPrompt;
+
+async function install() {
+    installBtnObj.style.backgroundColor = "#aaffaa"
+    await sleep(buttonPressMs)
+    installBtnObj.style.backgroundColor = "#55ff55"
+    await sleep(10)
+    deferredPrompt.prompt();
+    const { outcome } = await deferredPrompt.userChoice;
+    console.log(`User response to the install prompt: ${outcome}`);
+    deferredPrompt = null;
+}
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    installBtnObj.style.display = "unset";
+});
+
+window.addEventListener('appinstalled', () => {
+    deferredPrompt = null;
+    localStorage.setItem("isLeaving", "false")
+    window.location.reload();
+});
+
 const CACHE_NAME = 'Page-Cache';
 const urlsToCache = [
     "/",
-    "/favicon.png",
+    "/assets/github.png",
+    "/assets/vercel.png",
     "/game/",
     "/game/2x2/",
     "/game/2x2/scripts.js",
@@ -57,20 +103,52 @@ const urlsToCache = [
     "/icons/home.png",
     "/icons/settings.png",
     "/icons/trash.png",
-    "/logos/manifest-icon-192.png",
-    "/manifest.json",
     "/scripts.js",
     "/styles.css",
 ];
 
-function deleteCache(e) {
-    for (let i = 0; i < urlsToCache.length; i++) {
-        e.delete(urlsToCache[i]);
-    }
+function deleteCache() {
+    caches.delete(CACHE_NAME)
 }
 
 function loadCache(e) {
     e.addAll(urlsToCache);
+}
+
+function updateCache() {
+    if (isOnline) {
+        deleteCache();
+        caches.open(CACHE_NAME).then(function(e) {
+            loadCache(e);
+        })
+    }
+}
+
+function setScore() {
+    highScore = localStorage.getItem("HighScore");
+    score = localStorage.getItem("Score");
+
+    if (score == null || highScore == null) {
+        localStorage.setItem("HighScore", "0")
+        localStorage.setItem("Score", "0")
+    }
+
+    highScore = localStorage.getItem("HighScore");
+    score = localStorage.getItem("Score");
+
+
+    highScoreObj.innerHTML = `High Score: ${pad(highScore)}`;
+    scoreObj.innerHTML = `Score: ${pad(score)}`;
+}
+
+function getElmId() {
+    frameObj = document.getElementById("game");
+    scoreObj = document.getElementById("score");
+    highScoreObj = document.getElementById("high-score");
+    installBtnObj = document.getElementById("install");
+    resetbtnObj = document.getElementById("reset");
+    homebtnObj = document.getElementById("home");
+    root = document.querySelector(":root");
 }
 
 window.onbeforeunload = function() {
@@ -85,36 +163,14 @@ async function start() {
         navigator.serviceWorker.register('/service-worker.js')
     }
 
-    if (isOnline) {
-        caches.open(CACHE_NAME).then(function(e) {
-            deleteCache(e);
-            loadCache(e);
-        })
-
+    if (localStorage.getItem("isLeaving") != "false") {
+        updateCache();
     }
+    getElmId();
 
-    isLeaving = true;
-    frameObj = document.getElementById("game");
-    scoreObj = document.getElementById("score");
-    highScoreObj = document.getElementById("high-score");
-    root = document.querySelector(":root");
-    localStorage.setItem("isLeaving", "true");
+    installBtnObj.style.display = "none";
 
-    highScore = localStorage.getItem("HighScore");
-    score = localStorage.getItem("Score");
-
-    if (score == null || highScore == null) {
-        localStorage.setItem("HighScore", 0)
-        localStorage.setItem("Score", 0)
-    }
-
-    highScore = localStorage.getItem("HighScore");
-    score = localStorage.getItem("Score");
-
-
-    highScoreObj.innerHTML = `High Score: ${pad(highScore)}`;
-    scoreObj.innerHTML = `Score: ${pad(score)}`;
-
+    setScore();
     update();
 }
 
